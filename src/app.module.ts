2a66ca5c6488b19b71bdcb9d06fc0d59e11eef86
Juan Carlos Aristizabal <gongonut@gmail.com>
2023-07-08 16:29:30 -0500

    probar smpt

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,12 @@ import { MailerModule } from '@nestjs-modules/mailer';
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_USER}@cluster0.a6otiyi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     ),
+    /*
     MailerModule.forRootAsync({
       useFactory: () => ({
         transport: process.env.SMPT_EMAIL,
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
@@ -22,6 +25,7 @@ import { MailerModule } from '@nestjs-modules/mailer';
 
       }),
     }),
+    */
     QuoteModule,
 
   ],
