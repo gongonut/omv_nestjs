b195926f13da19c6317c249e61ae3cbbb9c509d7
gongonut <gongonut@gmail.com>
2023-09-01 09:38:25 -0500

    paginas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('webpage')
+  root(@Res() response): void {
+    // the homepage will load our index.html which contains angular logic
+    console.log(join(__dirname, '..', 'page/index.html'));
+    response.sendFile(join(__dirname, '..', 'page/index.html'));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+// import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { urlencoded, json } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, { cors: true });
-
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); // para servir html desde Express
+  // const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('api');
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   app.enableCors({
-    origin: '*',
+    origin: ['*'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
@@ -29,9 +31,19 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
 
+  
+  // para servir html desde Express
+  // https://stackoverflow.com/questions/54680459/serving-static-content-alongisde-angular-app
+  app.useStaticAssets(join(__dirname, '..', 'page'), { prefix: "/page/" });
+  app.useStaticAssets(join(__dirname, '..', 'app'), { prefix: "/app/" });
+  // app.setViewEngine('html');
+  
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
 
 // https://www.youtube.com/watch?v=K7TYj86Z3rY&t=3614s
 // https://www.youtube.com/watch?v=xRXHQlqA3Ak&t=2s min 12 para atlas mongo
+
+// create basic html index?
